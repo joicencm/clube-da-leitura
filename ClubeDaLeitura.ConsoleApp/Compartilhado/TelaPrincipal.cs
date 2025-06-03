a15c5c117cf38c5c58da42b825d0cfe397903936
Joice <joice2310241@gmail.com>
2025-06-03 20:35:43 -0300

    Implementada a Base

---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -0,0 +1,48 @@
+﻿namespace ClubeDaLeitura.ConsoleApp.Compartilhado;
+
+public class TelaPrincipal
+{
+    private char opcaoEscolhida;
+    public TelaPrincipal()
+    {       
+    }
+
+    public void ApresentarMenuPrincipal()
+    {
+        Console.Clear();
+
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("|           Clube da Leitura           |");
+        Console.WriteLine("----------------------------------------");
+
+        Console.WriteLine();
+
+        Console.WriteLine("1 - Controle de Amigos");
+        Console.WriteLine("2 - Controle de Caixa");
+        Console.WriteLine("3 - Controle de Revistas");
+        Console.WriteLine("4 - Controle de Empréstimos");
+        Console.WriteLine("S - Sair");
+
+        Console.WriteLine();
+
+        Console.Write("Escolha uma das opções: ");
+        opcaoEscolhida = Console.ReadLine()[0];
+    }
+
+    public TelaBase ObterTela()
+    {
+        if (opcaoEscolhida == '1')
+            return null;
+
+        else if (opcaoEscolhida == '2')
+            return null;
+
+        else if (opcaoEscolhida == '3')
+            return null;
+
+        else if (opcaoEscolhida == '4')
+            return null;
+
+        return null;
+    }
+}
